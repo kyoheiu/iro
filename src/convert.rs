8f8039c0fd3631717355bc6038ceee7217849083
Kyohei Uto <kyoheiu@outlook.com>
2022-07-23 06:33:36 +0900

    MVP

---
+++ src/convert.rs
@@ -0,0 +1,32 @@
+use super::display::*;
+
+pub fn convert(arg: String) {
+    let mut temp = "".to_string();
+    let mut v = vec![];
+    for (i, c) in arg.chars().enumerate() {
+        if i % 2 != 0 {
+            temp.push(c);
+            v.push(temp);
+            temp = "".to_string();
+        } else {
+            temp.push(c);
+        }
+    }
+
+    let rgb = v
+        .iter()
+        .map(|value| u8::from_str_radix(value, 16).unwrap())
+        .collect::<Vec<u8>>();
+
+    let hsl = convert_to_hsl(&rgb);
+
+    let bc = make_bc(&rgb);
+    let hex = make_hex(&rgb, &arg);
+
+    println!("{}", bc);
+    println!("{}", hex);
+    print_rgb(&rgb, &hsl);
+    print_rgb_ratio(&rgb, &hsl);
+    print_hsl(&rgb, &hsl);
+    println!("{}", bc);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
-use termion::color;
+mod color_names;
+mod convert;
+mod display;
+
+use convert::convert;
 
 fn main() {
-    let sample = "#7cb0a1";
-    let sample = sample.strip_prefix('#').unwrap();
-    let mut temp = "".to_string();
-    let mut v = vec![];
-    for (i, c) in sample.chars().enumerate() {
-        if i % 2 != 0 {
-            temp.push(c);
-            v.push(temp);
-            temp = "".to_string();
-        } else {
-            temp.push(c);
+    let mut args: Vec<String> = std::env::args().collect();
+    match args.len() {
+        1 => println!("show help."),
+        _ => {
+            args.remove(0);
+            for arg in args {
+                convert(arg);
+            }
         }
     }
-    println!("{:?}", v);
-
-    let rgb: Vec<u8> = v
-        .iter()
-        .map(|value| u8::from_str_radix(value, 16).unwrap())
-        .collect();
-    println!(
-        "{}█████████{:?}█{}",
-        color::Bg(color::Rgb(rgb[0], rgb[1], rgb[2])),
-        rgb,
-        color::Bg(color::Reset),
-    );
 }
